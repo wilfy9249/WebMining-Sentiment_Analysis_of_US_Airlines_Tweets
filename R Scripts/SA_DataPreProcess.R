133d62217f638d4cd9c1ada682ad8b80542c1b4d
Wilfred Castelino <wc335@njit.edu>
2018-11-13 22:22:12 -0500

    Data pre-processing and cleaning

--- R Scripts/SA_DataPreProcess.R
+++ R Scripts/SA_DataPreProcess.R
@@ -1,5 +1,7 @@
-##------------------------------------------------------------------##
-##-----------------GROUP1: WEB  MINING - FINAL PROJECT--------------##
+
+########################################################################
+#                GROUP1: WEB  MINING - FINAL PROJECT
+########################################################################
 
 ##IMPORT LIBRARIES
 library(readr)
@@ -13,4 +15,33 @@ str(airlineTweets)
 
 #Show observations in columns
 head(airlineTweets)
+
+##Create a DataFrame of Negative Sentiments
+negativeSubset = subset(airlineTweets, airline_sentiment == "negative")
+str(negativeSubset)
+
+##----------DATA CLEANING------------------------##
+
+##BUILD CORPUS AND CLEAN THE TEXT
+corpus = Corpus(VectorSource(negativeSubset$text))
+inspect(corpus[1:5])
+
+corpus <- tm_map(corpus, stripWhitespace)
+#strwrap(corpus[[8]])
+inspect(corpus[1:5])
+
+corpus <- tm_map(corpus, content_transformer(tolower))
+inspect(corpus[1:5])
+
+#Remove stopwords
+corpus = tm_map(corpus, removeWords, stopwords("english"))
+inspect(corpus[1:5])
+
+# Want to get rid of @virginamerica, @usairways, @united, @southwestair, @jetblue, @americanair
+corpus <- tm_map(corpus, function(x) gsub('@', 'ReplacedText', x))
+inspect(corpus[1:5])
+
+corpus = tm_map(corpus, removeWords, c("ReplacedTextvirginamerica","ReplacedTextusairways","ReplacedTextunited","ReplacedTextsouthwestair","ReplacedTextjetblue","ReplacedTextamericanair"))
+inspect(corpus[1:5])
+
 
